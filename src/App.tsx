fd1e73ce89a7d36b26dbe4fcf7f9bfbf7eb56f58
kim-seonkyu <seonkyu93@gmail.com>
2022-11-18 23:07:11 +0900

    react-query / react-query-devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Router from "./Router";
 
 const GlobalStyle = createGlobalStyle`
@@ -69,6 +70,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
